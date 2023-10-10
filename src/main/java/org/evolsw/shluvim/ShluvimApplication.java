0570861bceaf187b0f3f08f87d3baffe7f985c5b
eitanv <eitanv@gmail.com>
2023-10-11 01:50:38 +0300

    initial upload

---
+++ src/main/java/org/evolsw/shluvim/ShluvimApplication.java
@@ -0,0 +1,13 @@
+package org.evolsw.shluvim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShluvimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShluvimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/evolsw/shluvim/ShluvimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.evolsw.shluvim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShluvimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
