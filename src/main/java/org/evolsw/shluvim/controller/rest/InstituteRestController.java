dbf63d07f1b6044ef1af2036b1e10f6d2f95efa1
eitanv <eitanv@gmail.com>
2023-10-20 01:37:50 +0300

    Added updateCost and updateSalary

--- src/main/java/org/evolsw/shluvim/controller/rest/InstituteRestController.java
+++ src/main/java/org/evolsw/shluvim/controller/rest/InstituteRestController.java
@@ -5,9 +5,7 @@ import org.evolsw.shluvim.model.Institute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,5 +22,14 @@ public class InstituteRestController {
         List<Institute> allInstitutes = instututeServices.loadAll();
         System.out.println("All institutes: " + Arrays.toString(allInstitutes.toArray()));
         return new ResponseEntity<>(allInstitutes, HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}/cost={cost}")
+    public ResponseEntity<String> updateInstituteSalary(@PathVariable Long id, @PathVariable double cost) {
+        Institute institute = instututeServices.load(id);
+        institute.setBaseCost(cost);
+        instututeServices.save(institute);
+        System.out.println("Updated " + institute.getInstituteName() + "`s Cost to " + cost);
+        return new ResponseEntity<>("Updated cost", HttpStatus.OK);
     }
 }
--- src/main/java/org/evolsw/shluvim/controller/rest/InstructorRestController.java
+++ src/main/java/org/evolsw/shluvim/controller/rest/InstructorRestController.java
@@ -5,9 +5,7 @@ import org.evolsw.shluvim.model.Instructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,5 +22,14 @@ public class InstructorRestController {
         List<Instructor> allInstructors = instructorServices.loadAll();
         System.out.println("All instructors: " + Arrays.toString(allInstructors.toArray()));
         return new ResponseEntity<>(allInstructors, HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}/salary={salary}")
+    public ResponseEntity<String> updateInstructorCost(@PathVariable Long id, @PathVariable double salary) {
+        Instructor instructor = instructorServices.load(id);
+        instructor.setHourSalary(salary);
+        instructorServices.save(instructor);
+        System.out.println("Updated " + instructor.getName() + "`s Salary to " + salary);
+        return new ResponseEntity<>("Updated cost", HttpStatus.OK);
     }
 }
